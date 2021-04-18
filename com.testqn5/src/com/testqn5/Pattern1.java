a9419f3b9bef66ad3b98c27bd9c86674a254d8cc
Khaviya007 <80397018+Khaviya007@users.noreply.github.com>
2021-04-18 19:32:33 +0530

    Update Pattern1.java

--- com.testqn5/src/com/testqn5/Pattern1.java
+++ com.testqn5/src/com/testqn5/Pattern1.java
@@ -2,10 +2,14 @@ package com.testqn5;
 import java.lang.*;
 public class Pattern1 {
 	public static void main(String[] args) {		
-			
-			for(int i=0;i<=4;i++){
+			int n=4;
+			for(int i=0;i<=n;i++){
+				for(int j=1;j<=n-i;j++) {
+			        System.out.print(" ");
+			        }
 			int k=(int)Math.pow(11,i);
-			System.out.println(k);}
+			System.out.println(k);
+			}
 			}
 	}
 
