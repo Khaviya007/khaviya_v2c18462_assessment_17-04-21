84369dc1026412ab7c018ceb41c91bc86a6f6ee2
Khaviya007 <80397018+Khaviya007@users.noreply.github.com>
2021-04-19 10:29:27 +0530

    Update Override8.java

--- com.testqn5/src/com/testqn5/Override8.java
+++ com.testqn5/src/com/testqn5/Override8.java
@@ -7,11 +7,11 @@ class Parent{
 	}  
 }  			  
 public class Override8 extends Parent{  	
-		public void show() throws Exception
+		public void show() 
 		{  
 			System.out.println("subclass show method");  
 		} 		
-public static void main(String args[]){  
+public static void main(String args[]) throws Exception{  
 	Parent p=new Override8();  
 	p.show();  			    
 	}
